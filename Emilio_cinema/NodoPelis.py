17f79d5e97e1e17c76f9d7fd32cae88fae228017
Angel Enrique Alvarado Ruiz <2988764180101@ingenieria.usac.edu.gt>
2024-03-03 22:33:30 -0600

    removiendo archivos extras

--- Emilio_cinema/NodoPelis.py
+++
@@ -1,9 +0,0 @@
-class Informacion:
-    def __init__(self, ID, nombre, fecha, hora, categoria):
-        self.ID = ID
-        self.nombre = nombre
-        self.fecha = fecha
-        self.hora = hora
-        self.categoria = categoria
-        self.siguiente = None
-        self.anterior = None
